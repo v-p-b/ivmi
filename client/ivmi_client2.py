bae64feea9d8eeb574efe5a74274db077c12488c
buherator <b@zero1>
2017-01-24 18:25:12 +0100

    ExÅperimenting

--- client/ivmi_client2.py
+++ client/ivmi_client2.py
@@ -1,3 +1,5 @@
+from base64 import b64decode
+from binascii import hexlify
 from ivmi import *
 import json
 
@@ -8,7 +10,8 @@ ivmi=IVMI()
 
 ivmi.connect("tcp://127.0.0.1:22000","tcp://127.0.0.1:22001")
 
-print(repr(ivmi.init(DOMAIN,PROFILE)))
+info=ivmi.init(DOMAIN,PROFILE)
+print(repr(info))
 
 trap=IVMITrap()
 
@@ -19,15 +22,20 @@ trap.addr_type="RVA"
 trap.lookup_type="PID"
 trap.addr=ivmi.profile["$FUNCTIONS"]["KiDispatchException"] 
 
-#if trap.addr != 774598:
-#    ivmi.close()
-#    exit()
-
 print(repr(ivmi.add_trap(trap)))
 ivmi.resume()
-for i in range(0,30):
+for i in range(0,3):
     n=ivmi.get_notification(blocking=True)
     print(repr(n))
+    #info=ivmi.info()
+    #print(repr(info))
+   
+    """ 
+    if info['page_mode']=="IA32E":
+        #trap_frame=b64decode(ivmi.read_mem(4, n["regs"]["r8"], ivmi.profile["$STRUCTS"]["_KTRAP_FRAME"][0]))
+        #print(hexlify(trap_frame))
+        print(hexlify(b64decode(ivmi.read_mem(4,n["regs"]["rcx"],4))))
+    """
     ivmi.ack_notification()
     
 print(repr(ivmi.del_trap("KiDispatchException")))
