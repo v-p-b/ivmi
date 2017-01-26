41d12d0a8abed5700c626f58b95313bd0c9bfb55
buherator <b@zero1>
2017-01-26 12:39:13 +0100

    Updated command line example

--- examples/python/ivmi_client.py
+++ examples/python/ivmi_client.py
@@ -1,16 +1,37 @@
 import cmd
 import ivmi
+import _thread
 
 class IVMIShell(cmd.Cmd):
     intro="Welcome to iVMI!"
     prompt="ivmi# "
+    domain=None
+    control=None
+    notify=None
+    ns=[]
     ivmi=ivmi.IVMI()
+
+    def update_prompt(self):
+        parts=[]
+        if self.control != None:
+            parts.append("[%s]" % self.control)
+        if self.domain != None:
+            parts.append("[%s]" % self.domain)
+        self.prompt="ivmi%s# " % (''.join(parts))
+
+    def notify_loop(self):
+        while True:
+            n=self.ivmi.get_notification(blocking=True,ack=True)
+            self.ns.append(n)
 
     def do_connect(self, arg):
         'Connects to the remote iVMI queue specified as argument'
         args=arg.split()
         self.ivmi.connect(args[0],args[1])
-        self.prompt="ivmi[%s]# " % arg
+        _thread.start_new_thread(self.notify_loop,tuple())
+        self.control=args[0]
+        self.notify=args[1]
+        self.update_prompt()
     
     def do_list(self, arg):
         'List running domains'
@@ -23,6 +44,8 @@ class IVMIShell(cmd.Cmd):
         domain=args[0]
         profile=args[1]
         res=self.ivmi.init(domain,profile)
+        self.domain=domain
+        self.update_prompt()
         print(repr(res))
 
     def do_info(self, arg):
@@ -58,30 +81,35 @@ class IVMIShell(cmd.Cmd):
         print(repr(res))
 
     def do_add_trap(self, arg):
-        'Add trap TESTING'
+        'Add trap based on PID & module: add_trap <name> <PID> <module> <virtual address>'
         trap=ivmi.IVMITrap()
-        trap.name="KiDispatchException"
-        trap.module="ntoskrnl.exe"
-        trap.pid=4
-        trap.addr_type="RVA"
+        args=arg.split()
+
         trap.lookup_type="PID"
-        trap.addr=774598 # KiDispatchException From testx86.rekall.json !
+        trap.addr_type="RVA"
+        trap.name=str(args[0])
+        trap.pid=int(args[1])
+        trap.module=str(args[2])
+        trap.addr=int(args[3]) 
+        
         res=self.ivmi.add_trap(trap)
         print(repr(res))
 
     def do_get_notifications(self, arg):
-        n=self.ivmi.get_notification()
-        ret=[]
-        while n != None:
-            ret.append(n)
-            n=self.ivmi.get_notification()
-        print(repr(ret))
-
-
+        'Get received notifications'
+        nnum=0
+        while len(self.ns)>0:
+            print(repr(self.ns.pop()))
+            nnum+=1
+        print("Returned %d notifications." % nnum)
+            
     def do_close(self, arg):
         'Close introspection context'
         self.ivmi.close()
-        self.prompt="ivmi# "
+        self.control=None
+        self.notify=None
+        self.domain=None
+        self.update_prompt()
 
     def do_pause(self, arg):
         'Pause VM'
@@ -92,6 +120,12 @@ class IVMIShell(cmd.Cmd):
         'Resume VM'
         res=self.ivmi.resume()
         print(repr(res))
+
+    def do_exit(self, arg):
+        'Exit'
+        if self.control != None:
+            self.do_close(None)
+        return True
 
 if __name__ == '__main__':
     IVMIShell().cmdloop()
